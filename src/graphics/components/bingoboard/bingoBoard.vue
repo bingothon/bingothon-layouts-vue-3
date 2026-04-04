7fbc515015e24714580ad76971351a1e4f6cc0bd
lepelog <25211966+lepelog@users.noreply.github.com>
2026-04-04 17:18:05 +0200

    port basic version of the bingo reveal animation

--- src/graphics/components/bingoboard/bingoBoard.vue
+++ src/graphics/components/bingoboard/bingoBoard.vue
@@ -11,8 +11,8 @@
                 >
                     <td
                         v-for="(cell, j) in column"
-                        :id="'tile-' + i + '-' + j"
-                        :key="i + '' + j"
+                        :id="`tile-${i}-${j}`"
+                        :key="`${i}:${j}`"
                         class="square"
                     >
                         <div
@@ -45,6 +45,12 @@
             >
                 BINGO!
             </div>
+        </div>
+        <div
+            ref="revealSquare"
+            class="revealSquare"
+        >
+            BINGO
         </div>
         <div
             class="bingo-board-hide"
@@ -117,10 +123,6 @@
         setTimeout(() => (splashActivated.value = ''), 4000);
     }
 
-    const boardHidden = computed(() => {
-        return bingoboardMeta?.data?.boardHidden && !props.alwaysShown;
-    });
-
     onUnmounted(() => {
         nodecg.unlisten('showBingoAnimation', oldBundle, showBingoSplash);
     });
@@ -140,6 +142,120 @@
             return `marker marker${markerIndex} ${marker}square`;
         }
     }
+
+    /** start animation related */
+
+    const revealSquareElem = useTemplateRef<HTMLElement>('revealSquare');
+    const revealSquareX = ref(2);
+    const revealSquareY = ref(2);
+    const revealAnimationRunning = ref(false);
+
+    const boardHidden = computed(() => {
+        return bingoboardMeta?.data?.boardHidden && !props.alwaysShown;
+    });
+
+    watch(() => boardHidden.value, triggerRevealAnimation, { immediate: false });
+
+    function triggerRevealAnimation() {
+        if (boardHidden.value) return;
+        doRolling();
+    }
+
+    function calculateTransitionDuration(sequenceIndex: number) {
+        return (0.15 / (sequenceIndex + 1)) * 3;
+    }
+
+    function getRevealSequence(
+        rowCount: number,
+        columnCount: number
+    ): { row: number; col: number; direction: number }[] {
+        // we start in the bottom right corner, go up and then always follow the wall until we visited every tile
+        function move(col: number, row: number, direction: number): [number, number] {
+            switch (direction) {
+                case 0:
+                    return [col, row - 1];
+                case 1:
+                    return [col - 1, row];
+                case 2:
+                    return [col, row + 1];
+                default:
+                    return [col + 1, row];
+            }
+        }
+        const pool = new Set(
+            Array(rowCount)
+                .fill(null)
+                .flatMap((_, row) =>
+                    Array(columnCount)
+                        .fill(null)
+                        .map((_, col) => `${row}-${col}`)
+                )
+        );
+        let currentRow = rowCount;
+        let currentCol = columnCount - 1;
+        let direction = 0;
+        const seq = [];
+        for (let i = 0; i < rowCount * columnCount; i++) {
+            // try to move, if it doesn't work, turn
+            let [nextCol, nextRow] = move(currentCol, currentRow, direction);
+            if (!pool.has(`${nextRow}-${nextCol}`)) {
+                direction = (direction + 1) % 4;
+                [nextCol, nextRow] = move(currentCol, currentRow, direction);
+            }
+            currentRow = nextRow;
+            currentCol = nextCol;
+            seq.push({ row: currentRow, col: currentCol, direction });
+            pool.delete(`${currentRow}-${currentCol}`);
+        }
+        return seq.reverse();
+    }
+
+    async function doRolling() {
+        if (revealAnimationRunning.value) return;
+        revealAnimationRunning.value = true;
+        const revealSquare = revealSquareElem.value;
+        if (!revealSquare) {
+            return;
+        }
+        revealSquare.style.opacity = '1';
+        const revealSequence = getRevealSequence(rowCount.value, columnCount.value);
+        const squares = Array.from(bingoboardElem.value?.getElementsByClassName('square') ?? []) as HTMLElement[];
+        squares.forEach((square) => (square.style.opacity = '0'));
+        revealSquare.style.transitionDuration = '0s';
+        revealSquareX.value = revealSequence[0].col;
+        revealSquareY.value = revealSequence[0].row;
+        // delay to make initial position take effect
+        await delay(10);
+        try {
+            for (let i = 0; i < revealSequence.length; i++) {
+                const { col, row } = revealSequence[i];
+                const transitionDuration = calculateTransitionDuration(i);
+
+                revealSquare.style.transitionDuration = `${transitionDuration}s`;
+                revealSquareX.value = col;
+                revealSquareY.value = row;
+
+                await delay(transitionDuration * 1000);
+
+                const tile = squares[row * columnCount.value + col];
+                if (tile) {
+                    tile.style.opacity = '1';
+                }
+            }
+        } catch (error) {
+            console.log('Error in doRolling:', error);
+        } finally {
+            revealSquare.style.opacity = '0';
+
+            // Reveal all tiles
+            squares.forEach((cell) => (cell.style.opacity = '1'));
+            revealAnimationRunning.value = false;
+        }
+    }
+
+    function delay(ms: number): Promise<void> {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
 </script>
 
 <style lang="css">
@@ -155,6 +271,7 @@
         height: 100%;
         background: black;
         position: absolute;
+        top: 0;
         color: white;
         align-content: center;
         justify-content: center;
@@ -292,5 +409,45 @@
     .marker3 {
         right: 10%;
         bottom: 10%;
+    }
+    @keyframes tile-border {
+        0% {
+            border-color: transparent;
+        }
+        100% {
+            border-color: black;
+        }
+    }
+    .isRevealing {
+        opacity: 0;
+        animation: tile-border 0.5s ease-in-out;
+    }
+    .revealSquare {
+        position: absolute;
+        background-color: white;
+        height: calc(100% / v-bind(rowCount));
+        width: calc(100% / v-bind(columnCount));
+        top: calc(100% * v-bind(revealSquareY) / v-bind(rowCount));
+        left: calc(100% * v-bind(revealSquareX) / v-bind(columnCount));
+        opacity: 0;
+
+        transition-timing-function: linear;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 1.5em;
+        color: #333; /* Using the same color as .GameChip */
+        box-sizing: border-box;
+
+        /* Applying the ice cube styles from .GameChip */
+        padding: 3px 4px;
+        background-color: rgba(220, 240, 255, 1); /* Light blueish */
+        border-radius: 2px;
+        border: 1px solid rgba(18, 98, 138, 0.877); /* Slightly blueish border for that 'frozen' feel */
+
+        /* Ice-like gradient background using SVG */
+        background-image: url('data:image/svg+xml;utf8,<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient id="iceGradient" x1="0%" y1="0%" x2="100%" y2="100%"><stop offset="0%" style="stop-color:rgba(240, 255, 255, 0.7); stop-opacity:1" /><stop offset="100%" style="stop-color:rgba(200, 240, 255, 0.9); stop-opacity:1" /></linearGradient></defs><rect width="100%" height="100%" fill="url(%23iceGradient)" /></svg>');
+        box-shadow: 0px 0px 5px 2px rgba(8, 69, 92, 0.342); /* Subtle glow to add depth */
     }
 </style>
