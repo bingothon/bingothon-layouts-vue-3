40806c61f24eb8fcf4d65ae74cc2a4245f8e0ab8
lepelog <25211966+lepelog@users.noreply.github.com>
2026-09-09 22:48:07 +0200

    review streams

--- src/dashboard/streams/main.vue
+++ src/dashboard/streams/main.vue
@@ -1,57 +1,57 @@
 <template>
     <div
-        v-for="([stream, slotIdx], i) in twitchStreams"
-        :key="i"
+        v-for="(stream, slotIdx) in twitchStreamsBySlot"
+        :key="slotIdx"
+        class="q-mb-lg"
     >
-        <div class="stream-label">{{ getStreamLabel(i) }}:</div>
-        <QBtn
-            class="stream-mute"
-            dark
-            small
-            :disable="slotIdx === undefined"
-            :title="slotIdx === soundOnTwitchStream?.data ? 'currently unmuted' : 'currently muted'"
-            :icon="slotIdx === soundOnTwitchStream?.data ? 'volume_up' : 'volume_off'"
-            @click="muteChange(slotIdx)"
-        >
-        </QBtn>
-        <QBtn
-            icon="refresh"
-            class="stream-refresh"
-            dark
-            small
-            :disable="slotIdx === undefined"
-            @click="refresh(slotIdx)"
-        >
-        </QBtn>
-        <QBtn
-            class="stream-pause"
-            dark
-            small
-            :disable="slotIdx === undefined"
-            :icon="stream.paused ? 'play_arrow' : 'pause'"
-            @click="togglePlayPause(slotIdx)"
-        >
-        </QBtn>
-        <div>
-            <span>Vol: </span>
-            <QSlider
-                class="stream-volume"
-                :min="0"
-                :max="100"
-                :model-value="stream.volume * 100"
-                :disable="slotIdx === undefined"
-                @change="volumeChange(slotIdx, $event)"
-            />
-
+        <template v-if="stream">
+            <div>{{ getStreamLabel(stream) }}:</div>
+            <QBtnGroup push>
+                <QBtn
+                    dark
+                    small
+                    :title="slotIdx === soundOnTwitchStream?.data ? 'currently unmuted' : 'currently muted'"
+                    :icon="slotIdx === soundOnTwitchStream?.data ? 'volume_up' : 'volume_off'"
+                    @click="muteChange(slotIdx)"
+                >
+                </QBtn>
+                <QBtn
+                    icon="refresh"
+                    dark
+                    small
+                    @click="refresh(slotIdx)"
+                >
+                </QBtn>
+                <QBtn
+                    dark
+                    small
+                    :icon="stream.paused ? 'play_arrow' : 'pause'"
+                    @click="togglePlayPause(slotIdx)"
+                >
+                </QBtn>
+            </QBtnGroup>
+            <div class="row no-wrap flex-center">
+                <div>
+                    <QIcon
+                        size="md"
+                        name="volume_up"
+                    />
+                </div>
+                <QSlider
+                    class="q-ml-sm"
+                    :min="0"
+                    :max="100"
+                    :model-value="stream.volume * 100"
+                    @change="volumeChange(slotIdx, $event)"
+                />
+            </div>
             <QLinearProgress
-                :color="obsStreamAudioLevels[slotIdx ?? -1] > 95 ? 'red' : 'green'"
-                class="stream-volume-multiplier"
+                :color="obsStreamAudioLevels[slotIdx] > 95 ? 'red' : 'green'"
                 min="0"
                 max="100"
-                :disable="slotIdx === undefined"
-                :value="obsStreamAudioLevels[slotIdx ?? -1] ?? 0"
+                :value="obsStreamAudioLevels[slotIdx] ?? 0"
             />
-        </div>
+        </template>
     </div>
 </template>
 
@@ -71,24 +71,16 @@
 
     useHead({ title: '(Twitch) Streams' });
 
-    const twitchStreams = computed(
+    const twitchStreamsBySlot = computed(
         () =>
-            streamsReplicant?.data?.map((s, sIdx): [TwitchStream, number | undefined] => [
-                s,
-                playerIdxToSlot.value[sIdx]
-            ]) ?? []
+            playerSlotsReplicant?.data?.slots.map((slot) =>
+                streamsReplicant?.data?.find((s) => s.playerId === slot.playerId)
+            ) ?? []
     );
     const players = computed(() => runDataActiveRunReplicant?.data?.teams.flatMap((t) => t.players.flat()) ?? []);
-    const playerIdxToSlot = computed(() =>
-        Object.fromEntries(
-            players.value.map((p, pIdx) => {
-                const slotIdx = playerSlotsReplicant?.data?.slots.findIndex((s) => s.playerId === p.id);
-                return [pIdx, slotIdx === -1 ? undefined : slotIdx];
-            })
-        )
-    );
+
     const obsStreamAudioLevels = computed(() =>
-        twitchStreams.value.map((_, idx) => {
+        twitchStreamsBySlot.value.map((_, idx) => {
             const sourceName =
                 obsStreamSourceTypeReplicant?.data === 'obsTwitchPlayer'
                     ? `twitch-stream-${idx}`
@@ -97,45 +89,32 @@
         })
     );
 
-    const getStreamLabel = computed(() => (idx: number) => {
-        const stream = streamsReplicant?.data?.[idx];
+    const getStreamLabel = computed(() => (stream: TwitchStream) => {
         const channelName =
             obsStreamSourceTypeReplicant?.data === 'obsSrtMediasource' ? stream?.srtChannel : stream?.channel;
-        return `${idx}: ${players.value[idx].name} (${channelName})`;
+        const playerName = players.value.find((p) => p.id === stream.playerId)?.name;
+        return `${playerName} (${channelName})`;
     });
     function volumeChange(id: number | undefined, newVal: number) {
-        if (id !== undefined) {
-            const newVolume = newVal / 100;
-            nodecg.sendMessageToBundle('streams:setStreamVolume', oldBundle, { id, volume: newVolume });
-        }
+        const newVolume = newVal / 100;
+        nodecg.sendMessageToBundle('streams:setStreamVolume', oldBundle, { id, volume: newVolume });
     }
 
-    function muteChange(id: number | undefined) {
-        if (id !== undefined) {
-            if (soundOnTwitchStream?.data === id) {
-                nodecg.sendMessageToBundle('streams:setSoundOnTwitchStream', oldBundle, -1);
-            } else {
-                nodecg.sendMessageToBundle('streams:setSoundOnTwitchStream', oldBundle, id);
-            }
+    function muteChange(id: number) {
+        if (soundOnTwitchStream?.data === id) {
+            nodecg.sendMessageToBundle('streams:setSoundOnTwitchStream', oldBundle, -1);
+        } else {
+            nodecg.sendMessageToBundle('streams:setSoundOnTwitchStream', oldBundle, id);
         }
     }
 
     function togglePlayPause(id: number | undefined) {
-        if (id !== undefined) {
-            nodecg.sendMessageToBundle('streams:toggleStreamPlayPause', oldBundle, id);
-        }
+        nodecg.sendMessageToBundle('streams:toggleStreamPlayPause', oldBundle, id);
     }
 
     function refresh(id: number | undefined) {
-        if (id !== undefined) {
-            nodecg.sendMessageToBundle('streams:refreshStream', oldBundle, id);
-        }
+        nodecg.sendMessageToBundle('streams:refreshStream', oldBundle, id);
     }
 </script>
 
-<style>
-    .stream-volume-multiplier {
-        margin-top: -28px;
-        margin-bottom: 10px;
-    }
-</style>
+<style></style>
